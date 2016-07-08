669e80c2ffdad07083749ac77d7e33798cf76f67
Phan Nguyen <phannh.uetk58@gmail.com>
2016-07-08 16:07:40 +0700

    edit_profile_show_user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,9 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:index, :edit, :update, :show]
+  before_action :find_by_id, only: [:show, :update, :edit]
+  before_action :correct_user, only: [:edit, :update]
+
   def show
-    @user = User.find_by params[:id]
   end
 
   def new
@@ -11,14 +14,47 @@ class UsersController < ApplicationController
     @user = User.new user_params
     if @user.save
       flash[:success] = t "static_pages.home_title"
-      redirect_to @user
+      redirect_to login_url
     else
       render :new
     end
   end
 
+  def edit
+  end
+
+  def index
+    @users = User.order(updated_at: :desc).paginate page: params[:page],
+      per_page: Settings.users.per_page
+  end
+
+  def update
+    if @user.update_attributes user_params
+      flash[:success] = t "flash.update"
+      redirect_to @user
+    else
+      render :edit
+    end
+  end
+
+  private
   def user_params
     params.require(:user).permit :name, :email, :password,
       :password_confirmation
+  end
+
+  def logged_in_user
+    unless logged_in?
+      flash[:danger] = t "flash.action"
+      redirect_to login_url
+    end
+  end
+
+  def correct_user
+    redirect_to root_url unless @user.current_user? current_user
+  end
+
+  def find_by_id
+    @user = User.find_by id: params[:id]
   end
 end
