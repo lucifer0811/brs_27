7996ec3b7e6c09fc58c0ff7b5e8309540251bfc4
huanlv <lehuan1993@gmail.com>
2016-07-15 13:53:20 +0700

    delete request

--- app/controllers/admin/requests_controller.rb
+++ app/controllers/admin/requests_controller.rb
@@ -1,6 +1,19 @@
 class Admin::RequestsController<ApplicationController
+before_action :logged_in_user, only: [:show]
+before_action :check_admin, only: [:show, :destroy]
 
   def show
     @requests=Request.all.order(updated_at: :desc).paginate page: params[:page]
+  end
+
+  def destroy
+    @request=Request.find_by id: params[:id]
+    unless @request.nil?
+      @request.destroy
+      flash[:success]= t "request.destroyed"
+    else
+      flash[:notice]= t "request.not_destroyed"
+    end
+    redirect_to admin_requests_path
   end
 end
