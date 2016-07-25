f56418466a762f5a79e152455fbc0dc0c1b5cc2b
Phan Nguyen <phannh.uetk58@gmail.com>
2016-07-25 11:01:57 +0700

    UI_2

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,7 +1,7 @@
 class ActivitiesController < ApplicationController
   def index
     @activities = Activity.order("created_at DESC").paginate page: params[:page],
-      per_page: Settings.per_page
+      per_page: Settings.users.per_page
   end
   def show
     @activity = Activity.find_by id: params[:id]
--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,5 +2,10 @@ class CategoriesController < ApplicationController
   def index
     @categories = Category.order(created_at: :asc).paginate page: params[:page],
       per_page: Settings.users.per_page
+    @books = Book.all
+  end
+  def show
+    @category = Category.all.find_by id: params[:id]
+    @books = @category.books
   end
 end
