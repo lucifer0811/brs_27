fc74973d514f794e0f709120ff2f93fc73e82dbb
Phan Nguyen <phannh.uetk58@gmail.com>
2016-07-28 04:20:41 +0700

    fix

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,3 +15,8 @@
 //= require bootstrap
 //= require jquery.raty
 //= require ratyrate
+$(document).on 'ready page:load', ->
+  $('div.alert-danger').delay(3000).slideUp()
+  $('div.alert-success').delay(3000).slideUp()
+  cancel_function = ->
+    window.history.back()
